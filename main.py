116a034a1c4a96865fb1c80d3b55cbd1e19b0f91
Alastair Rayner <alli0@me.com>
2015-11-27 17:23:36 +0000

    added button to connect to server

--- main.py
+++ main.py
@@ -1,9 +1,28 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
+from kivy.uix.button import Button
+from kivy.network.urlrequest import UrlRequest
+from kivy.properties import NumericProperty, ReferenceListProperty,\
+ObjectProperty, StringProperty
+from kivy.vector import Vector
+from kivy.clock import Clock
+import cgitb
+cgitb.enable()
+
+
+
 
 
 class AsteroidsGame(Widget):
-    pass
+
+    hello_world = StringProperty()
+    def update_string(self, req, results):
+        self.hello_world = results
+
+
+    def button_pressed(self):
+        req = UrlRequest("http://bsccg01.ga.fal.io/hello_world/?user=Class", self.update_string)
+
 
 
 class AsteroidsApp(App):
@@ -11,5 +30,15 @@ class AsteroidsApp(App):
         return AsteroidsGame()
 
 
+
+
+
+#class Ship(Widget):
+ #   velocity_x = numericproperty(0)
+ #   velocity_y = numericproperty(0)
+
+
+
+
 if __name__ == '__main__':
-    PongApp().run()
+    AsteroidsApp().run()
