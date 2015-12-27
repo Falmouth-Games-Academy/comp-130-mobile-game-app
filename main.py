539bd0557bbf589688072deb3502376d4ccf4de6
Alastair Rayner <alli0@me.com>
2015-12-27 06:15:23 +0000

    Small Update not much implemented
    Still learning how things work and relate to each other, Not much
    progress made, just a quick backup

--- main.py
+++ main.py
@@ -8,36 +8,23 @@ from kivy.uix.gridlayout import GridLayout
 from kivy.uix.boxlayout import BoxLayout
 from kivy.graphics.vertex_instructions import (Rectangle, Ellipse, Triangle, Line)
 from kivy.graphics.context_instructions import Color
+from kivy.graphics.instructions import InstructionGroup, Canvas
 from kivy.uix.screenmanager import ScreenManager, Screen, FadeTransition
 from kivy.lang import Builder
 from kivy.network.urlrequest import UrlRequest
 from kivy.properties import NumericProperty, ReferenceListProperty,\
 ObjectProperty, StringProperty, ListProperty
+from kivy.core.window import Window
 from kivy.vector import Vector
 from kivy.clock import Clock
-import random
+from kivy.animation import Animation
+from random import random
 import cgitb
 cgitb.enable()
 
 
-
-
 class StartScreen(Screen):
-    def __init__(self, **kwargs):
-        super(StartScreen, self).__init__(**kwargs)
 
-        colour = [random.random() for i in range(3)] + [1]
-
-    #username_input = self.ids.username_input
-    #username_input.text = "text"
-
-
-    # def on_enter(instance, value):
-    #     print('User pressed enter in', instance, value)
-    #
-    # textinput = TextInput(text='name')
-    # textinput.bind(on_text_validate=on_enter)
-    value = StringProperty()
 
 
     def on_text(self, username_text):
@@ -47,9 +34,6 @@ class StartScreen(Screen):
 
     textinput = TextInput()
     textinput.bind(text=on_text)
-
-
-
 
 
     hello_world = StringProperty()
@@ -62,13 +46,76 @@ class StartScreen(Screen):
     def button_pressed(self):
         username = StringProperty()
         username = self.username_text
-
-
         req = UrlRequest("http://bsccg08.ga.fal.io/hello_world/?user=" + username + "." , self.update_string)
   # req = UrlRequest("http://bsccg08.ga.fal.io/hello_world/?user=%s(username)" , self.update_string)
 
-class Game(Screen):
+
+
+
+
+class Game(Screen, Widget):
     pass
+
+class Asteroid(Widget):
+    #velocity = ListProperty([1,1])
+
+    # def __init__(self, **kwargs):
+    #     super(Asteroid, self).__init__(**kwargs)
+    #     Clock.schedule_interval(self.anim_to_pos, 1/60.)
+    #
+    #
+    # def update(self, *args):
+    #     pass
+
+    #
+    #     self.x += self.velocity[0]
+    #     self.y += self.velocity[1]
+    #
+    #
+    #     if self.x < (self.x + self.width) > Window.width:
+    #         self.velocity[0] *= -1
+    #     if self.y < (self.x + self.height) > Window.height:
+    #         self.velocity[1] *= -1
+    #
+    x_touch = 1
+    y_touch = 1
+
+
+    def anim_to_pos(self, xtouch, ytouch):
+        Animation.cancel_all(self)
+        random_x = random() * (Window.width - 100)
+        random_y = random() * (Window.height - 100)
+
+        #print(random_x, random_y, Window.width - self.width)
+
+        anim = Animation(x=xtouch, y=ytouch, duration=1, t='out_elastic')
+        print(self.x_touch, self.y_touch)
+        anim.start(self)
+
+
+
+
+    def on_touch_down(self, touch):
+        self.x_touch, self.y_touch = touch.pos
+        xtouch = self.x_touch
+        ytouch = self.y_touch
+
+        #
+        # with self.canvas:
+        #     touch.ud["Rectangle"] = Rectangle(pos=(touch.x, touch.y))
+        #
+
+
+        if self.collide_point(*touch.pos):
+            self.anim_to_pos(xtouch, ytouch)
+
+
+
+
+
+
+
+
 
 class MyScreenManager(ScreenManager):
     pass
