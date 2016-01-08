d48eb8e03e07a63c76287894f3c4da8e558f7c76
teasterbrook17 <tseasterbrook17@gmail.com>
2016-01-08 21:54:05 +0000

    Mobile App
    I dont know why it will not compile.

--- COMP130 kivy project/main.py
+++ COMP130 kivy project/main.py
@@ -6,13 +6,35 @@ from kivy.properties import NumericProperty, ReferenceListProperty,\
     ObjectProperty
 from kivy.vector import Vector
 from kivy.clock import Clock
+from kivy.uix.label import Label
+from kivy.uix.textinput import TextInput
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.screenmanager import ScreenManager, Screen, FadeTransition
+from kivy.lang import Builder
+from kivy.animation import Animation
 
-class snakeGame(Widget):
+
+class Menu(Screen):
     pass
 
+class snakeGame(Screen,Widget):
+    pass
+
+class HighScoreMenu(Screen):
+    pass
+
+class TheScreenManager(ScreenManager):
+    pass
+
+class Monster(Widget):
+    def on_touch_down(self, touch):
+        super(Monster, self).on_touch_down(touch)
+
+snakeGameKivy = Builder.load_file("snake.kv")
 class snakeApp(App):
     def build(self):
-        return snakeGame()
+        game = snakeGameKivy
+        return game
 
 if __name__ == "__main__":
     snakeApp().run()
