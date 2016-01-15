bcb8f733834ad46f002e3cf431a6e0bf9413db7b
Warwick New(wn182882) <mistercheeez@hotmail.co.uk>
2016-01-15 11:55:01 +0000

    minor tweaks to UI, player weapons implemented, player attack half implemented

--- main.py
+++ main.py
@@ -1,38 +1,93 @@
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.button import Button, ButtonBehavior
-from kivy.uix.image import Image
-from kivy.clock import Clock
 import random
+from kivy.uix.widget import Widget
+from kivy.properties import StringProperty
 from kivy.core.audio import SoundLoader
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen
 
 # Declaring screens for use in screen manager
 class BattleScreen(Screen):
-    def check_for_monster(self):
-        try:
-            return self.monster
-        except:
-            self.monster = self.random_stats()
-            return self.monster
+    pass
+#    playerDescription = StringProperty()
+#
+ #   def update_descriptions(self, playerDesc, monsterDesc):
+  #      self.playerDescription.text = playerDesc
+   #     self.monsterDescription.text = monsterDesc
 
-    def check_for_player(self):
-        try:
-            return self.player
-        except:
-            self.player = self.random_stats()
-            return self.player
 
-    def make_stat_text(self, title):                                        # title changes who's stats are displayed
-        if title == 'Monster:':
-            character = self.check_for_monster()
-        elif title == 'Player:':
-            character = self.check_for_player()
-        else:
-            character = self.random_stats()
-        for stat in range(0, len(character)):
-            title = title + '\n\n' + str(character[stat][0]) + ' = ' + str(character[stat][1])
+class MenuScreen(Screen):
+    pass
+
+
+class LogInScreen(Screen):
+    pass
+
+
+class MainApp(App):
+    def build(self):
+        self.load_sounds()
+        self.generate_player()
+        self.playerDescription = StringProperty()
+        self.playerDescription = self.get_player_stats()
+        self.generate_monster()
+        self.monsterDescription = StringProperty()
+        self.monsterDescription = self.get_monster_stats()
+        return self.load_screens()
+# player and monster generation and modification
+
+    def attack_monster(self):
+        damage = self.generate_player_damage()
+        stat = self.monster[self.find_stat(self.monster, 'Hit Points')][1]
+        self.monster[self.find_stat(self.monster, 'Hit Points')][1] = stat - damage
+        self.end_turn()
+
+    def end_turn(self):
+        self.monsterDescription = self.get_monster_stats
+        print self.monsterDescription
+        print self.monster
+
+    def generate_player_damage(self):
+        weaponDamage = self.player[self.find_stat(self.player, 'Weapon')][3]
+        standardDamage = self.player[self.find_stat(self.player, 'Strength')][1]
+        averageDamage = weaponDamage + standardDamage
+        randomDamage = int(averageDamage * 0.75) + random.randint(0, int(averageDamage/2))
+        return randomDamage
+
+    def find_stat(self, character, stat):                                                                        # returns the location of a stat or item in a player or monster
+        for statIndex in range(0, len(character)):
+            if character[statIndex][0] == stat:
+                return statIndex
+        return 'Stat not found'
+
+    # used to call in game player stats to print
+    def get_player_stats(self):
+        title = 'Player:'
+        player = self.player
+        for stat in range(0, len(player)):
+            if player[stat][0] != 'Weapon':
+                title = title + '\n\n' + str(player[stat][0]) + ' = ' + str(player[stat][1])
+            else:
+                title = title + '\n\n' + str(player[stat][0]) + ' = ' + str(player[stat][1]) \
+                        + ' ' + str(player[stat][2]) + ' ' + str(player[stat][3])
         return title
+
+    # used to call in game monster stats to print
+    def get_monster_stats(self):
+        title = 'Monster: '
+        monster = self.monster
+        for stat in range(0, len(monster)):
+            title = title + '\n\n' + str(monster[stat][0]) + ' = ' + str(monster[stat][1])
+        return title
+
+    def generate_player(self):
+        self.player = self.random_stats()
+        self.player.append(('Potions', random.randint(0, 5)))
+        self.player.append(self.random_weapon())
+        return self.player
+
+    def generate_monster(self):
+        self.monster = self.random_stats()
+        return self.monster
 
     def random_stats(self):
         hp = random.randint(14, 22)
@@ -44,35 +99,35 @@ class BattleScreen(Screen):
         will = random.randint(1, 10)
         damageResistance = random.randint(0, 5)
         character = []
-        character.append(('HP', hp))
+        character.append(['Hit Points', hp])
         character.append(('Strength', strength))
         character.append(('Dexterity', dexterity))
         character.append(('Will', will))
         character.append(('Toughness', damageResistance))
-        character = character
         return character
 
-class MenuScreen(Screen):
-    pass
+    def random_weapon(self):
+        weaponType = [('Rod', 1), ('Spear', 2), ('Axe', 3), ('Sword', 4)]
+        weaponMaterial = [('Wood', 0), ('Copper', 1), ('Iron', 2), ('Adamantine', 3)]
+        type = weaponType[random.randint(0, len(weaponType) - 1)]
+        material = weaponMaterial[random.randint(0, len(weaponMaterial) - 1)]
+        weapon = ['Weapon', material[0], type[0], type[1] + material[1]]
+        return weapon
 
-# The application class
-class MainApp(App):
-    def build(self):
-        self.load_sounds()
-        self.text = 'here is some\nlovely text'
-        return self.load_screens()
+# loading files into memory
 
     # This function loads screens from the kv file
     def load_screens(self):
         sm = ScreenManager()
         sm.add_widget(MenuScreen(name='menu'))
         sm.add_widget(BattleScreen(name='battle'))
+        sm.add_widget(LogInScreen(name='login'))
         return sm
 
     def load_sounds(self):
         self.sounds = {}
-        fname = 'sound3.wav'
-        self.sounds[0] = SoundLoader.load(fname)
+        fileName = 'sound3.wav'
+        self.sounds[0] = SoundLoader.load(fileName)
 
     def play_sound1(self):
         sound = self.sounds.get(0)
@@ -82,3 +137,4 @@ class MainApp(App):
 
 if __name__ == '__main__':
     MainApp().run()
+
