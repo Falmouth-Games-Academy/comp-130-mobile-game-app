3b4f609fdf834463373892ee025cdef0f758a04f
Maddie <mk186442@falmouth.ac.uk>
2016-01-23 14:21:21 +0000

    Reorganised Leaderboard.py

--- kivy app/Leaderboard.py
+++ kivy app/Leaderboard.py
@@ -5,37 +5,33 @@ from kivy.uix.gridlayout import GridLayout
 from kivy.network.urlrequest import UrlRequest
 from kivy.uix.textinput import TextInput
 from kivy.uix.dropdown import DropDown
-from kivy.base import runTouchApp
+from kivy.uix.widget import Widget
 
 
-class LeaderboardApp(App):
-    choice_button = Button(text="Get results")
-    results_label = Label(text="High scores", font_size='20sp')
-    textinput = TextInput(text='Enter username here.', multiline=False)
-    dropdown = DropDown()
-    dropdown_button = Button(text="Select an option")
-    dropdown_button.bind(on_release=dropdown.open)
-    layout = GridLayout(rows=5)
-    drop_down_options = ["Top 10 Scores", "Add New User", "Top scores for user", "Change User Name"]
-    URL_choice = "None"
-    """
-    def __init__(self, main_level, main_score):
-        score = main_level
-        level = main_score
-        print score + level"""
+class LeaderBoardLayout(Widget):
+    def __init__(self, **kwargs):
+        super(LeaderBoardLayout, self).__init__(**kwargs)
+        self.choice_button = Button(text="Get results")
+        self.results_label = Label(text="High scores", font_size='20sp')
+        self.name_input = TextInput(text='Enter username here.', multiline=False)
+        self.choices_dropdown = DropDown()
+        self.dropdown_button = Button(text="Select an option")
+        self.dropdown_button.bind(on_release=self.choices_dropdown.open)
+        self.layout = GridLayout(rows=5)
+        self.drop_down_options = ["Top 10 Scores", "Add New User", "Top scores for user"]
+        self.URL_choice = "None"
 
-    def build(self):
-
+    def create_layout(self):
         self.choice_button.bind(on_press=self.callback)
 
         for d in self.drop_down_options:
             btn = Button(text=d, size_hint_y=None, height=44)
-            btn.bind(on_release=lambda btn: self.dropdown.select(btn.text))
-            self.dropdown.add_widget(btn)
-        self.dropdown.bind(on_select=lambda instance, x: setattr(self.dropdown_button, 'text', x))
+            btn.bind(on_release=lambda btn: self.choices_dropdown.select(btn.text))
+            self.choices_dropdown.add_widget(btn)
+        self.choices_dropdown.bind(on_select=lambda instance, x: setattr(self.dropdown_button, 'text', x))
 
         self.layout.add_widget(self.dropdown_button)
-        self.layout.add_widget(self.textinput)
+        self.layout.add_widget(self.name_input)
         self.layout.add_widget(self.choice_button)
         self.layout.add_widget(self.results_label)
         return self.layout
@@ -48,8 +44,8 @@ class LeaderboardApp(App):
         self.results_label.text = str(results)
 
     def callback(self, event):
-        playername = self.textinput.text[:3]
-        self.textinput.text = playername
+        playername = self.name_input.text[:3]
+        self.name_input.text = playername
         self.results_label.text = "Getting scores"
 
         if self.dropdown_button.text == "Top 10 Scores":
@@ -58,10 +54,14 @@ class LeaderboardApp(App):
             self.URL_choice = 'http://bsccg04.ga.fal.io/new_user.py?playername=' + playername
         elif self.dropdown_button.text == "Top scores for user":
             self.URL_choice = 'http://bsccg04.ga.fal.io/users_high_score.py?playername=' + playername
-        # Add other options
-
-        print self.URL_choice
 
         request = UrlRequest(self.URL_choice, self.got_top_10)
 
-# LeaderboardApp().run()
+
+class LeaderBoardApp(App):
+    def build(self):
+        making_layout = LeaderBoardLayout()
+        the_layout = making_layout.create_layout()
+        return the_layout
+
+LeaderBoardApp().run()
