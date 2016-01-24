e3f334e85b04463f5ba1a0608b8f575e85b327ed
sw180283 <samwillsacc@gmail.com>
2016-01-24 14:20:38 +0000

    Commented code with docstrings

--- metal_owl/metal_owl.py
+++ metal_owl/metal_owl.py
@@ -2,22 +2,25 @@
 
 import random # import random allows for random generation of values
 
-from kivy.app import App # import abilities of kivy to run app
-from kivy.uix.widget import Widget # import widgets from kivy
-#from kivy.graphics import Color, Rectangle # commented out as image used instead
-from kivy.core.window import Window # import window adjustment
-from kivy.uix.image import Image # import use of images
-from kivy.clock import Clock # import kivy clock
-from kivy.uix.label import Label # import kivy label
-from kivy.core.audio import SoundLoader # import sound usage
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.core.window import Window
+from kivy.uix.image import Image
+from kivy.clock import Clock
+from kivy.uix.label import Label
+from kivy.core.audio import SoundLoader
 
-# set the sound source
+'''
+Set the location and names for each of the sounds.
+'''
 sfx_flap = SoundLoader.load("Resources/audio/flap.wav")
 sfx_score = SoundLoader.load("Resources/audio/score.wav")
 sfx_die = SoundLoader.load("Resources/audio/Owl_music.wav")
 
-# create the main menu using the widget to add the background, ground, label
-# super makes sure the widget is initialised
+'''
+Create the main menu and add the background, ground and label.
+Super makes sure the widget is initialised.
+'''
 class Menu(Widget):
     def __init__(self):
         super(Menu, self).__init__()
@@ -25,28 +28,32 @@ class Menu(Widget):
         self.size = self.children[0].size
         self.add_widget(Ground(source="Resources/images/metal_ground.png"))
         self.add_widget(Label(center=self.center, text="METAL" + "\n" + " GEAR" + "\n" + " OWL"))
-        #self.add_widget(Label(pos=(self.center_x-44, self.center_y+4), text="METAL" + "\n" + " GEAR" + "\n" + " OWL"))
         self.add_widget(Label(pos=(self.center_x-44, self.center_y-140), text="Tap to start"))
-# when click down remove the menu widget and add the game widget as the new parent
+    '''
+    When the user pressed down on touch screen or mouse click remove the main menu widget and add the game widget.
+    '''
     def on_touch_down(self, *ignore):
         parent = self.parent
         parent.remove_widget(self)
         parent.add_widget(Game())
 
-# when calling sprites use the image import, **kwargs stop from messing with functionality, size is set to the size of the image
+'''
+Set the class sprite as an image and name the size of it the size of the texture of the image.
+'''
 class Sprite(Image):
     def __init__(self, **kwargs):
         super(Sprite, self).__init__(**kwargs)
         self.size = self.texture_size
 
-# the pipe class used as widget, one set the top and bottom image as different pipes, add each as widget
-# set top image position to x, with y being 5.5 * the size of the player
-# set the score to false as default
+'''
+This widget controls both the top and the bottom spikes as one set.
+The top of the image is 5.5 x the size of the player.
+'''
 class Pipe(Widget):
     def __init__(self, pos):
         super(Pipe, self).__init__(pos=pos)
         self.top_image = Sprite(source="Resources/images/spike_top.png")
-        self.top_image.pos = (self.x, self.y + 99)
+        self.top_image.pos = (self.x, self.y + 5.5 * 24)
         self.add_widget(self.top_image)
         self.bottom_image = Sprite(source="Resources/images/spike_bottom.png")
         self.bottom_image.pos = (self.x, self.y - 340)#- self.bottom_image.height)
@@ -54,10 +61,11 @@ class Pipe(Widget):
         self.width = self.bottom_image.width
         #self.width = self.top_image.width
         self.scored = False
-
-# update the pipe widget with the x position moving 2 pixels
-# set the top and bottom image as the updating self.x value
-# if the pipe is less than 0 position remove the pipe widget
+    '''
+    Update the spikes and moves them 2 pixels to the left.
+    Set the top spike and bottom spike as the new x position.
+    If the spike x position is less than 0 remove the widget.
+    '''
     def update(self):
         self.x -=  2
         self.top_image.x = self.bottom_image.x = self.x
@@ -65,10 +73,12 @@ class Pipe(Widget):
         if self.right < 0:
             self.parent.remove_widget(self)
 
-# all pipes
-# the pipes class changes the calling of pipes
-# update the pipes by time
-# add a pipe at random height and width for gap position
+'''
+Controls all spikes.
+Add spike is set to 0 which is descreases over time.
+When less than 0 set a random height and x position.
+Place the new spike widget and randomise the add spike variable countdown.
+'''
 class Pipes(Widget):
     add_pipe = 0
     def update(self, dt):
@@ -82,9 +92,10 @@ class Pipes(Widget):
             self.add_widget(Pipe(pos=(x, y)))
             self.add_pipe = random.uniform(0.5,4.0)
 
-
-# add background widget where image sprite is used
-# duplicate the image with x position set at end of last image
+'''
+Set the background image as a sprite and set it's size to the size of the image.
+Create a duplicate of the background image and set the x position to the width of the background.
+'''
 class Background(Widget):
     def __init__(self, source):
         super(Background, self).__init__()
@@ -94,9 +105,11 @@ class Background(Widget):
         self.image_dupe = Sprite(source=source, x=self.width)
         self.add_widget(self.image_dupe)
 
-# update the image and duplicate
-# move left by 2 pixels
-# when the image is less than 0 place image duplicate at the end of the last image
+    '''
+    Update the image and the duplicate by moving 2 pixels to the left.
+    When the image is less or equal to 0 set the image to the 0 x position
+    And the image duplicate x position to the width of the image so appears offscreen.
+    '''
     def update(self):
         self.image.x -= 2
         self.image_dupe.x -= 2
@@ -105,8 +118,10 @@ class Background(Widget):
             self.image.x = 0
             self.image_dupe.x = self.width
 
-# the bird class is called as a sprite with the velocity and gravity of player is set
-# initial value of velocity is set to 0 and gravity is -0.3
+'''
+The bird is called as a sprite and the velocity is set to 0 which is the movement in the y axis.
+The gravity is set to -0.3 as it is a suitable fall speed
+'''
 class Bird(Sprite):
     def __init__(self, pos):
         super(Bird, self).__init__(source="atlas://Resources/images/owl_anim/wing-up", pos=pos)
@@ -114,28 +129,33 @@ class Bird(Sprite):
         self.gravity = -.3
         #self.grounded = False
 
-# the max velocity is set and the y position of the player is set as the updating velocity
-# the image of the bird is dependent on the velocity
+    '''
+    The velocity and gravity are added to make the combined direction of the y axis.
+    The altas changes the image of the bird depending on the velocity of the bird.
+    '''
     def update(self):
         self.velocity_y += self.gravity
         self.velocity_y = max(self.velocity_y, -10)
         self.y += self.velocity_y
         if self.velocity_y < -4:
-            self.source = "atlas://Resources/images/owl_anim/wing-up" #"Resources/kivy-game-dev/flappy/images/owl.png" "atlas://Resources/kivy-game-dev/flappy/images/bird_anim/wing-up"
+            self.source = "atlas://Resources/images/owl_anim/wing-up"
         elif self.velocity_y < 0:
-            self.source = "atlas://Resources/images/owl_anim/wing-mid" #"Resources/kivy-game-dev/flappy/images/owl.png"  "atlas://Resources/kivy-game-dev/flappy/images/bird_anim/wing-mid"
+            self.source = "atlas://Resources/images/owl_anim/wing-mid"
 
-# when clicked set the velocity and change the bird image and make flap sound
-# on event the velocity is set to 5.5
+    '''
+    When the screen is tapped or mouse is clicked increase the upward force, change the image of the bird and make sound.
+    The velocity increase is 5.5 as it is a suitable y axis movement.
+    '''
     def on_touch_down(self, *ignore):
         self.velocity_y = 5.5
-        self.source = "atlas://Resources/images/owl_anim/wing-down" # "Resources/kivy-game-dev/flappy/images/owl.png"  "atlas://Resources/kivy-game-dev/flappy/images/bird_anim/wing-down"
+        self.source = "atlas://Resources/images/owl_anim/wing-down"
         sfx_flap.play()
 
-
-# the ground is called as a sprite with the ground being updated moving it's x position
-# move left two pixels per update
-# if the x value is less than -24 then add 24
+'''
+The ground is called as a sprite and updated to move 2 pixels left.
+When the x position is less than -24 then set the reset the position back.
+This is a number that is suitable to the length of the ground sprite.
+'''
 class Ground(Sprite):
     def update(self):
         self.x -= 2
@@ -143,16 +163,13 @@ class Ground(Sprite):
         if self.x < -24:
             self.x += 24
 
-# the game is called as a widget
-# the background is set and added as a widget
-# the game size is set to the size of the background
-# the pipes are added as a widget under the ground so that the ground covers bottom edge
-# the ground is added as a widget
-# the score label is centered but at the top of the screen with a default text of "0" score
-# the over label of "Game Over" text has it's opacity set to 0 so is invisible
-# the bird player is set (x, y) = (20, half of screen) and added as a widget
-# the clock calls for updates at 60 times per second
-# game_over is default set to false and the score is default set to 0
+'''
+All the widgets needed for the main game are added to the game widget.
+Set the clock to update.
+Set the label opacity for game over to be 0.
+Set the bird and spike position.
+Set game over to default false and score to 0.
+'''
 class Game(Widget):
     def __init__(self):
         super(Game, self).__init__()
@@ -176,12 +193,9 @@ class Game(Widget):
         #self.grounded = False
         self.score = 0
 
-# update by delta time
-# check for game_over
-# check for bird collision with ground and change game_over accordingly
-# check for bird collision with top and bottom pipe and change game_over accordingly
-# if the position of the pipe is behind the player, set score +1 and set scored to true
-# set the score label to reflect the string score and play score sound
+    '''
+    Update te game and each of the widgets.
+    '''
     def update(self, dt):
         if self.game_over:
             return
@@ -202,14 +216,20 @@ class Game(Widget):
         #if self.bird.collide_widget(self.ground):
             #self.game_over = True
 
-
-        #bounce off top and bottom
+        '''
+        If the bird is less than 40 in the y axis, which is the level of the ground, then set the y to 40.
+        If the bird goes above the top of the screen -40 then set the top of the player to be -44 from the top.
+        This stops the player from going off the screen.
+        '''
         if (self.bird.y < 40):
             self.bird.y = 40
         if (self.bird.top > self.height-40):
-            #self.bird.velocity_y *= -1.0
             self.bird.top = self.height-44
 
+        '''
+        If the player collides with the spikes then set game over to true.
+        If the spikes goes past the player then set scored to true and add to score.
+        '''
         for pipe in self.pipes.children:
             if pipe.top_image.collide_widget(self.bird):
                 self.game_over = True
@@ -221,32 +241,34 @@ class Game(Widget):
                 self.score_label.text = str(self.score) + " m"
                 #sfx_score.play()
 
-
-# play die sound and change opacity of the over_label to visible
+        '''
+        If game over is true then change opacity of game over label to 1 and play music.
+        Set the next event to tap or click.
+        '''
         if self.game_over:
             sfx_die.play()
             self.over_label.opacity = 1
             self.bind(on_touch_down=self._on_touch_down)
 
-# on click down remove the game widget and add the menu widget
+    '''
+    To remove game widget and return to main menu click or tap.
+    '''
     def _on_touch_down(self, *ignore):
         parent = self.parent
         parent.remove_widget(self)
         #parent.add_scores(Scores())
         parent.add_widget(Menu())
 
-
-# call as the app where the top widget called is the menu with the window size set to it's game size
+'''
+This is the game app.
+Set the window size.
+'''
 class Metal_OwlApp(App):
     def build(self):
-        #return Game(size=Window.size)
-        #return Game()
-        #game = Game()
         top = Widget()
         top.add_widget(Menu())
         Window.size = top.children[0].size
         return top
-        #return game
 
 if __name__ == "__main__":
     Metal_OwlApp().run()
